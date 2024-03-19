77127629c8cc07daebd712eb92362ad37d89be91
simone-danzeri <s.danzeri@icloud.com>
2024-03-19 15:56:33 +0100

    feat: aggiunto quanti numeri l'utente ha indovinato

--- js/script.js
+++ js/script.js
@@ -24,7 +24,22 @@ const clock = setInterval(function () {
   }
 }, 1000);
 // Dopo che i numeri ed il timer sono scomparsi devo far uscire 5 prompt che chiedano all'utente di inserire un numero
-const guesses = setTimeout(manyPrompts, 30500); 
+let myScore = document.querySelector("#score");
+const correctNumber = [];
+let currPromp;
+let score = 0;
+const myTimeout = setTimeout(numInput, 31000);
+function numInput(){
+    for(let i=0; i<5; i++){
+        currPromp = parseInt(prompt(`Inserisci il ${i+1}° numero`));
+        // Controllo quanti numeri l'utente ha indovinato
+        if(randArray.includes(currPromp)){
+            correctNumber[score] = currPromp;
+            score++;
+        }
+    }
+    myScore.innerHTML = `Ne hai indovinati ${score} su  5`;
+}
 
 // FUNCTIONS
 
@@ -34,32 +49,32 @@ const guesses = setTimeout(manyPrompts, 30500);
 // numMax -> numero intero massimo da generare
 // return: un array di arrayLength numeri random
 function generateRandomArray(arrayLength, numMin, numMax) {
-  // Creiamo un array vuoto
-  const randomNumbersArray = [];
-
-  // finche non ci sono arrayLength numeri nell'array:
-  while (randomNumbersArray.length < arrayLength) {
-    // genero un numero random
-    const randNumber = getRndInteger(numMin, numMax);
-    // se il numero random non esiste nell'array lo pusho
-    if (!randomNumbersArray.includes(randNumber)) {
-      randomNumbersArray.push(randNumber);
+    // Creiamo un array vuoto
+    const randomNumbersArray = [];
+    
+    // finche non ci sono arrayLength numeri nell'array:
+    while (randomNumbersArray.length < arrayLength) {
+        // genero un numero random
+        const randNumber = getRndInteger(numMin, numMax);
+        // se il numero random non esiste nell'array lo pusho
+        if (!randomNumbersArray.includes(randNumber)) {
+            randomNumbersArray.push(randNumber);
+        }
     }
-  }
-
-  return randomNumbersArray;
+    
+    return randomNumbersArray;
 }
 
 // Funzione che mi crea un determinato numero (num) di prompt all'utente
 // num -> un numero intero di prompt per l'utente
 // return: tanti prompt quanto è il valore di num
-function manyPrompts(num) {
+function manyPrompts() {
     let prompts;
     for(let i = 0; i < 5; i++) { // al posto del 5 si deve mettere num ma per questo esercizio mi serviva 5
         prompts = parseInt(prompt('Dimmi un numero'));
         console.log(prompts);
     }
-}
+}  
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
